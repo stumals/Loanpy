dd7a0eac548ad435f587dd31d30201bdedbf67ca
stu mals <stuartmals23@gmail.com>
2022-03-17 00:45:01 -0400

    edit to test file

--- tests/test_loanpy.py
+++ tests/test_loanpy.py
@@ -6,21 +6,21 @@ import numpy as np
 #%%
 
 @pytest.fixture
-def loan_data():
+def loan_data1():
     return Loan(300000, .03, 30)
 
-def test_total_pmt(loan_data):
-    assert np.allclose(loan_data.interest + loan_data.principal, loan_data.pmt), "Interest + Principal DOES NOT equal Payment"
+def test_total_pmt(loan_data1):
+    assert np.allclose(loan_data1.interest + loan_data1.principal, loan_data1.pmt), "Interest + Principal DOES NOT equal Payment"
     
-def test_end_bal(loan_data):
-    assert np.allclose(loan_data.end_bal[-1], 0), "Ending balance after last payment DOES NOT equal 0.0"
+def test_end_bal(loan_data1):
+    assert np.allclose(loan_data1.end_bal[-1], 0), "Ending balance after last payment DOES NOT equal 0.0"
 
-def test_beg_bal(loan_data):
-    assert np.allclose(loan_data.beg_bal[0], loan_data.amt), "Beginning loan balance DOES NOT equal loan amount"
+def test_beg_bal(loan_data1):
+    assert np.allclose(loan_data1.beg_bal[0], loan_data1.amt), "Beginning loan balance DOES NOT equal loan amount"
     
-def test_amort_len(loan_data):
-    assert len(loan_data.periods)==len(loan_data.pmts)==len(loan_data.interest)==len(loan_data.principal)==len(loan_data.beg_bal) \
-       ==len(loan_data.end_bal)==loan_data.nper, "Calculated arrays DO NOT all equal number of periods"
+def test_amort_len(loan_data1):
+    assert len(loan_data1.periods)==len(loan_data1.pmts)==len(loan_data1.interest)==len(loan_data1.principal)==len(loan_data1.beg_bal) \
+       ==len(loan_data1.end_bal)==loan_data1.nper, "Calculated arrays DO NOT all equal number of periods"
     
 
 

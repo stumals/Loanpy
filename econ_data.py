502c41ebc15a0ecc87d86ab0c9a566325d3b7572
stu mals <stuartmals23@gmail.com>
2022-06-08 01:48:57 -0400

    updated econ_data.py for dates, more to do

--- econ_data.py
+++ econ_data.py
@@ -4,11 +4,15 @@ import numpy as np
 import os
 import requests
 from io import StringIO
-from datetime import date
+from datetime import date, datetime, timedelta
+import time
 import matplotlib.pyplot as plt
 #%%
 
-def get_mortgage_rates(date_index=False):
+def mortgage_rates(start_date=str(date.today()-timedelta(days=365)), end_date=str(date.today()), date_index=True):
+
+    assert type(start_date) == str and type(end_date) == str, "Enter start and end dates as 'YYYY-MM-DD'"
+
     url = 'https://www.freddiemac.com/pmms/docs/PMMS_history.csv'
     r = requests.get(url)
     df = pd.read_csv(StringIO(r.text)).fillna(0)
@@ -16,23 +20,36 @@ def get_mortgage_rates(date_index=False):
     assert 'date' in df.columns, 'date not a column name'
 
     for c in df.columns:
-        print(c)
+        #print(c)
         if c == 'date':
             df[c] = pd.to_datetime(df[c])
         else:
             df[c] = df[c].replace(r'^\s*$', 0, regex=True)
             df[c] = df[c].astype(float)
     
+    df = df.rename(columns={'pmms30':'30yr_FRM', 'pmms30p':'30yr_FeesPts', 
+                            'pmms15':'15yr_FRM', 'pmms15p':'15yr_FeesPts', 
+                            'pmms51':'5-1_ARM', 'pmms51p':'5-1_ARM_FeesPts',
+                            'pmms51m':'5-1_ARM_margin', 'pmms51spread':'30yrFRM_ARM_spread'})
+    
+    start = pd.to_datetime(start_date)
+    end = pd.to_datetime(end_date)
+
+    df = df[(df['date']>=start) & (df['date']<=end)]
+    
     if date_index:
         df.set_index('date', inplace=True)
     return df
 
+#%%
+def fedfunds_rates(start_date=str(date.today()-timedelta(days=365)), end_date=str(date.today()), 
+                   rates=['avg_rate_index'], ascending=True, date_index=True):
 
-def get_fedfunds_rate(start_date, end_date=str(date.today()), rates=['avg_rate_index'], ascending=True, date_index=False):
     if ascending: asc_code = '1'
     else: asc_code = '-1'
-    rate_type = {'unsecured_rates':'500,505', 'secured_rates':'510,515,520', 'avg_rate_index':'525', 
-                'EFFR':'500', 'OBFR':'505', 'TGCR':'510', 'BGCR':'515', 'SOFR':'520', 'SOFRAI':'525'}
+    rate_type = {'unsecured_rates':'500,505', 'secured_rates':'510,515,520',
+                 'avg_rate_index':'525', 'EFFR':'500', 'OBFR':'505', 'TGCR':'510', 
+                 'BGCR':'515', 'SOFR':'520', 'SOFRAI':'525'}
     assert type(start_date) == str and type(end_date) == str, "enter date args as strings in 'YYYY-MM-DD'"
 
     assert type(rates) == list, 'rates arg must be of type list'
@@ -58,15 +75,24 @@ def get_fedfunds_rate(start_date, end_date=str(date.today()), rates=['avg_rate_i
 
     url = 'https://markets.newyorkfed.org/read?startDt={}&endDt={}&eventCodes={}'\
     '&productCode=50&sort=postDt:{},eventCode:1&format=xml'.format(start_date, end_date, rt_input, asc_code)
-    print(url)
+    #print(url)
     r = requests.get(url)
     df = pd.read_xml(StringIO(r.text), xpath='//rate')
+    df = df.rename(columns={'effectiveDate':'date'})
     if date_index:
-        return df.set_index('effectiveDate')
+        return df.set_index('date')
     return df
 
+#%%
 
-def get_ustreasury_rates(year, type='Daily Treasury Par Yield Curve Rates', date_index=False):
+def ustreasury_rates(start_date=str(date.today()-timedelta(days=365)), end_date=str(date.today()),
+                     type='Daily Treasury Par Yield Curve Rates', date_index=True):
+    
+    #assert type(start_date) == str and type(end_date) == str, "Enter start and end dates as 'YYYY-MM-DD'"
+
+    start_year = int(start_date[:4])
+    end_year = int(end_date[:4])
+    years = list(range(start_year, end_year+1))
 
     rate_type = {'Daily Treasury Par Yield Curve Rates':'daily_treasury_yield_curve',
                 'Daily Treasury Bill Rates':'daily_treasury_bill_rates',
@@ -80,15 +106,63 @@ def get_ustreasury_rates(year, type='Daily Treasury Par Yield Curve Rates', date
                                      'Daily Treasury Long-Term Rates\n'\
                                      'Daily Treasury Par Real Yield Curve Rates\n'\
                                      'Daily Treasury Real Long-Term Rates'
-    url = 'https://home.treasury.gov/resource-center/data-chart-center/interest-rates/' \
-    'daily-treasury-rates.csv/{}/all?type={}&' \
-    'amp;field_tdr_date_value={}&amp;page&amp;_format=csv'.format(year, rate_type[type], year)
+    df = pd.DataFrame()
+    for year in years:
+        url = 'https://home.treasury.gov/resource-center/data-chart-center/interest-rates/' \
+        'daily-treasury-rates.csv/{}/all?type={}&' \
+        'amp;field_tdr_date_value={}&amp;page&amp;_format=csv'.format(year, rate_type[type], year)
+        r = requests.get(url)
+        df1 = pd.read_csv(StringIO(r.text))
+        df1.Date = pd.to_datetime(df1.Date)
+        df1 = df1.rename(columns={'Date':'date'})
+        df = pd.concat([df, df1], axis=0)
 
-    r = requests.get(url)
-    df = pd.read_csv(StringIO(r.text))
-    df.Date = pd.to_datetime(df.Date)
+    df = df.sort_values('date')
+    df = df.reset_index(drop=True)
+
     if date_index:
-        return df.set_index('Date')
+        return df.set_index('date')
     return df
 
+#%%
+#int(str(date.today()-timedelta(days=365))[:4])
+ustreasury_rates()
+# df = pd.DataFrame()
+# df1 = mortgage_rates(start_date='2022-01-01', end_date='2022-01-31', date_index=False)
+# df2 = mortgage_rates(start_date='2022-02-01', end_date='2022-02-28', date_index=False)
+# df = pd.concat([df, df1], axis=0)
+# df = pd.concat([df, df2], axis=0)
+# df
+
+
+#%%
+def get_stock_data(start_date, end_date=str(date.today()), ticker='%5EGSPC', date_index=True, freq='1wk'):
+    url = 'https://query2.finance.yahoo.com/v8/finance/chart/{}'.format(ticker)
+    start=int(time.mktime(time.strptime(str(start_date), '%Y-%m-%d')))
+    end=int(time.mktime(time.strptime(str(end_date), '%Y-%m-%d')))
+    params = {"period1": start, "period2": end}
+    params["interval"] = freq
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Custom user agent'})
+    data_json = session.get(url, params=params).json()
+
+    data = {'price': data_json['chart']['result'][0]['indicators']['quote'][0]['close']}
+    data['date'] = pd.to_datetime(data_json['chart']['result'][0]['timestamp'], unit='s')
+    
+    df = pd.DataFrame(data)
+    if ticker == '%5EGSPC':   
+        df = df.rename(columns={'price':'SP500'})
+    else:
+        df = df.rename(columns={'price':ticker})
+    if date_index:
+        return df.set_index('date')
+
+    return df
+#%%
+
+url = 'https://www.fhfa.gov/HPI_master.csv'
+r = requests.get(url).text
+r
+
+#%%
 
